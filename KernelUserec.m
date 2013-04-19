e540a671197a4bb86a15cfd4b0692c708a2ba472
haibara <zhenlei.yan@gmail.com>
2013-04-19 15:10:05 +0800

    add kernel pitf model

---
+++ KernelUserec.m
@@ -0,0 +1,92 @@
+function ret = KernelUserec(utf_tensor,train_friend_cases,user_tag_cell,train_user_friend,test_user_friend,num_feature)
+
+regular = 0.00005;
+learn_rate = 0.005;
+init_mean = 0;
+init_std = 0.01;
+num_iteration = 100;
+num_neg_samples = 10;
+epsion = 0.01;
+num_user = size(utf_tensor,1);
+num_tag = size(utf_tensor,2);
+num_frd = size(utf_tensor,3);
+top_N = 100;
+kernel_type = 1;
+%% train
+fprintf('train pitf...\n');
+start_t = clock;
+[U,T,F_U,F_T] = TrainKernelPITF(train_friend_cases,num_user,num_tag,num_frd,num_feature,init_mean,init_std,regular,learn_rate,num_iteration,num_neg_samples,epsion,kernel_type);
+train_t = etime(clock,start_t);
+disp(['cost time: ',num2str(train_t)]);
+fprintf('train pitf end...\n');
+
+%% predict top friends based on each tag
+model_file = [num2str(month(now)) '_' num2str(day(now)) '_' num2str(hour(now)) '_' num2str(minute(now)) '_' num2str(second(now)) '_dim' num2str(num_feature) '_pitf_model'];
+save(['./pitf-data/' model_file '.mat'],'U','T','F_U','F_T');
+fprintf('predict...\n');
+predict_dim = 4;
+test_users = unique(test_user_friend(:,1));
+num_frds = size(F_U,1);
+per_user_predict_cell = cell(length(test_users));
+for i = 1:length(test_users)
+    tags = user_tag_cell{test_users(i)};
+    per_tag_predict_cell = cell(length(tags),1);
+    for j = 1:length(tags)
+        temp_ret = PredictTopFrds(test_users(i),tags(j));
+        temp_ret_count = size(temp_ret,1);
+        per_tag_predict_cell{j,1} = [test_users(i)*ones(1,temp_ret_count);tags(j)*ones(1,temp_ret_count);temp_ret']';        
+    end
+    per_tag_predict = DecellPredictRet(per_tag_predict_cell);
+    per_user_predict_cell{i,1} = per_tag_predict;
+end
+per_user_predict = DecellPredictRet(per_user_predict_cell);
+%% fusion rank
+userec = FusionRankByRRF(test_user_friend,per_user_predict,top_N);
+save(['./pitf-data/' model_file '_predict.mat'],'userec');
+fprintf('predict end...\n');
+%% evaluate result
+fprintf('evaluate...\n');
+[precison,recall,fmeasure,mrr,map] = Evaluate(test_user_friend,userec);
+fprintf('predict end...\n');
+ret = cell(6,1);
+ret{1} = precison;
+ret{2} = recall;
+ret{3} = fmeasure;
+ret{4} = mrr;
+ret{5} = map;
+ret{6} = train_t;
+save(['./pitf-data/' model_file '_evaluate.mat'],'ret');
+saveas(gcf,['./pitf-data/' model_file '.fig']);
+%% sub functions
+    function ret = PredictTopFrds(uid,tid)
+        predict_frds = zeros(num_frds,2);
+        train_friends = train_user_friend(train_user_friend(:,1) == uid,2);
+        for ii = 1:num_frds
+            if ~isempty(find(train_friends == ii,1))
+                continue;
+            end
+            predict_frds(ii,1) = ii;
+            temp = 0;
+            for jj = 1:num_feature
+                temp = temp + U(uid,jj)*F_U(ii,jj) + T(tid,jj)*F_T(ii,jj);
+            end
+            predict_frds(ii,2) = temp;
+        end
+        predict_frds(predict_frds(:,1) == 0,:) = [];
+        ret = sortrows(predict_frds,-2);
+    end
+
+    function ret = DecellPredictRet(predict_cell)
+        cell_count = size(predict_cell,1);
+        ctr = 0;
+        for ii = 1:cell_count
+            ctr = ctr + size(predict_cell{ii},1);
+        end
+        ret = zeros(ctr,predict_dim);
+        index = 1;
+        for ii = 1:cell_count
+            ret(index:index+size(predict_cell{ii},1)-1,:) = predict_cell{ii};
+            index = index + size(predict_cell{ii},1);
+        end
+    end
+end
---
+++ TrainKernelPITF.m
@@ -0,0 +1,182 @@
+function [U,T,F_U,F_T] = TrainKernelPITF(frd_cases,num_user,num_tag,num_frd,num_feature,init_mean,init_std,regular,learn_rate,num_iteration,num_neg_samples,epsion,kernel_type,varargin)
+%% parameters: kernel type
+% 1. linear kernel <w,h>
+% 2. polynomial kernel (1+<w,h>)^d
+% 3. RBF kernel exp(-||w-h||^2 / 2delta^2)
+% 4. logistic kernel phi(b+<w,h>);
+%%
+% Initialize matrix
+U = GetNormMatrix(num_user,num_feature,init_mean,init_std);
+T = GetNormMatrix(num_tag,num_feature,init_mean,init_std);
+F_U = GetNormMatrix(num_frd,num_feature,init_mean,init_std);
+F_T = GetNormMatrix(num_frd,num_feature,init_mean,init_std);
+
+num_frd_case = size(frd_cases,1);
+num_iteration_neg_samples = num_frd_case * num_neg_samples;
+
+%% train
+for i = 1:num_iteration
+    fprintf('iteration %d...\n',i);
+    start_t = clock;
+    old_U = U;
+    old_T = T;
+    old_FU = F_U;
+    old_FT = F_T;
+    for j = 1:num_iteration_neg_samples
+        p = ceil(rand * num_frd_case);
+        uid = frd_cases{p,1};
+        tid = frd_cases{p,2};
+        fid_p = frd_cases{p,3};
+        fid_n = DrawNegSample(num_frd,frd_cases{p,4});
+        Learn(uid,tid,fid_p,fid_n,kernel_type);
+    end
+    diff = CalcIterationDiff(old_U,old_T,old_FU,old_FT);
+    subplot(2,2,1);
+    plot(i,diff(1));
+    hold on;
+    subplot(2,2,2);
+    plot(i,diff(2));
+    hold on;
+    subplot(2,2,3);
+    plot(i,diff(3));
+    hold on;
+    subplot(2,2,4);
+    plot(i,diff(4));
+    hold on;
+    disp(['cost time: ',num2str(etime(clock,start_t))]);
+    fprintf('iteration diff: %f,%f,%f,%f\n',diff(1),diff(2),diff(3),diff(4));
+    if sum(diff) < epsion
+        break;
+    end
+end
+hold off;
+%% sub functions
+    function local_diff = CalcIterationDiff(old_U,old_T,old_FU,old_FT)
+        local_diff = zeros(4,1);
+        local_diff(1) = norm(U-old_U,'fro');
+        local_diff(2) = norm(T-old_T,'fro');
+        local_diff(3) = norm(F_U-old_FU,'fro');
+        local_diff(4) = norm(F_T-old_FT,'fro');        
+    end
+    function local_score = PredictCaseScore(uid,tid,fid,kernel_type)
+        switch kernel_type
+            case 1
+                uf_score = MyDot(U(uid,:),F_U(fid,:));
+                tf_score = MyDot(T(tid,:),F_T(fid,:));
+            case 2                
+                d = varargin{1};
+                uf_score = (1+MyDot(U(uid,:),F_U(fid,:)))^d;
+                tf_score = (1+MyDot(T(tid,:),F_T(fid,:)))^d;
+            case 3
+                delta2 = varargin{1}^2;
+                uf_score = exp(-MyDot((U(uid,:)-F_U(fid,:)),(U(uid,:)-F_U(fid,:))) / (2*delta2));
+                tf_score = exp(-MyDot((T(uid,:)-F_T(fid,:)),(T(uid,:)-F_T(fid,:))) / (2*delta2));                
+            case 4
+                b = varargin{1};
+                uf_score = 1 / (1 + exp(-b-MyDot(U(uid,:),F_U(fid,:))));
+                tf_score = 1 / (1 + exp(-b-MyDot(T(tid,:),F_T(fid,:))));                
+            otherwise
+                error(['invalid kernel type:' num2str(kernel_type)]);
+        end
+        local_score = uf_score + tf_score;
+    end
+    
+    function ret = DrawNegSample(num_frd,fids_p)
+        neg = ceil(rand*num_frd);
+        while ~isempty(find(fids_p == neg, 1))
+            neg = ceil(rand*num_frd);
+        end 
+        ret = neg;
+    end
+
+    function Learn(uid,tid,fid_p,fid_n,kernel_type)
+        local_score_p = PredictCaseScore(uid,tid,fid_p,kernel_type);
+        local_score_n = PredictCaseScore(uid,tid,fid_n,kernel_type);
+        normalizer = GetLNSigmodPartialLoss(local_score_p - local_score_n);
+        switch kernel_type
+            case 1
+                for ii = 1:num_feature
+                    uf = U(uid,ii);
+                    tf = T(tid,ii);
+                    fuf_p = F_U(fid_p,ii);
+                    fuf_n = F_U(fid_n,ii);
+                    ftf_p = F_T(fid_p,ii);
+                    ftf_n = F_T(fid_n,ii);
+                    U(uid,ii) = U(uid,ii) + learn_rate * (normalizer * (fuf_p - fuf_n) - regular * uf);
+                    T(tid,ii) = T(tid,ii) + learn_rate * (normalizer * (ftf_p - ftf_n) - regular * tf);
+                    F_U(fid_p,ii) = F_U(fid_p,ii) + learn_rate * (normalizer * uf - regular * fuf_p);
+                    F_U(fid_n,ii) = F_U(fid_n,ii) + learn_rate * (normalizer * (-uf) - regular * fuf_n);
+                    F_T(fid_p,ii) = F_T(fid_p,ii) + learn_rate * (normalizer * tf - regular * ftf_p);
+                    F_T(fid_n,ii) = F_T(fid_n,ii) + learn_rate * (normalizer * (-tf) - regular * ftf_n);            
+                end
+            case 2
+                d = varargin{1};
+                ufu_p_score = d*(1+MyDot(U(uid,:),F_U(fid_p,:)))^(d-1);
+                ufu_n_score = d*(1+MyDot(U(uid,:),F_U(fid_n,:)))^(d-1);
+                tft_p_score = d*(1+MyDot(T(tid,:),F_T(fid_p,:)))^(d-1);
+                tft_n_score = d*(1+MyDot(T(tid,:),F_T(fid_n,:)))^(d-1);                
+                for ii = 1:num_feature
+                    uf = U(uid,ii);
+                    tf = T(tid,ii);
+                    fuf_p = F_U(fid_p,ii);
+                    fuf_n = F_U(fid_n,ii);
+                    ftf_p = F_T(fid_p,ii);
+                    ftf_n = F_T(fid_n,ii);
+                    U(uid,ii) = U(uid,ii) + learn_rate *(normalizer * (ufu_p_score*fuf_p-ufu_n_score*fuf_n) - regular * uf);
+                    T(tid,ii) = T(tid,ii) + learn_rate *(normalizer * (tft_p_score*ftf_p-tft_n_score*ftf_n) - regular * tf);                    
+                    F_U(fid_p,ii) = F_U(fid_p,ii) + learn_rate * (normalizer * (uf*ufu_p_score) - regular * fuf_p);
+                    F_U(fid_n,ii) = F_U(fid_n,ii) + learn_rate * (normalizer * (-uf*ufu_n_score) - regular * fuf_n);
+                    F_T(fid_p,ii) = F_T(fid_p,ii) + learn_rate * (normalizer * (tf*tft_p_score) - regular * ftf_p);
+                    F_T(fid_n,ii) = F_T(fid_n,ii) + learn_rate * (normalizer * (-tf*tft_n_score) - regular * ftf_n);
+                end
+            case 3
+                delta2 = varargin{1}^2;
+                ufu_p_score = exp(-MyDot((U(uid,:)-F_U(fid_p,:)),(U(uid,:)-F_U(fid_p,:))) / (2*delta2));
+                ufu_n_score = exp(-MyDot((U(uid,:)-F_U(fid_n,:)),(U(uid,:)-F_U(fid_n,:))) / (2*delta2));                
+                tft_p_score = exp(-MyDot((T(tid,:)-F_T(fid_p,:)),(T(tid,:)-F_T(fid_p,:))) / (2*delta2));
+                tft_n_score = exp(-MyDot((T(tid,:)-F_T(fid_n,:)),(T(tid,:)-F_T(fid_n,:))) / (2*delta2));                
+                for ii = 1:num_feature
+                    uf = U(uid,ii);
+                    tf = T(tid,ii);
+                    fuf_p = F_U(fid_p,ii);
+                    fuf_n = F_U(fid_n,ii);
+                    ftf_p = F_T(fid_p,ii);
+                    ftf_n = F_T(fid_n,ii);                    
+                    U(uid,ii) = U(uid,ii) + learn_rate * (normalizer * (ufu_n_score*(uf-fuf_n)-ufu_p_score*(uf-fuf_p))/delta2 - regular * uf);
+                    T(tid,ii) = T(tid,ii) + learn_rate * (normalizer * (tft_n_score*(tf-ftf_n)-tft_p_score*(tf-ftf_p))/delta2 - regular * tf);                  
+                    F_U(fid_p,ii) = F_U(fid_p,ii) + learn_rate * (normalizer * (ufu_p_score*(uf-fuf_p)/delta2) - regular * fuf_p);
+                    F_U(fid_n,ii) = F_U(fid_n,ii) + learn_rate * (normalizer * (-ufu_n_score*(uf-fuf_n)/delta2) - regular * fuf_n);
+                    F_T(fid_p,ii) = F_T(fid_p,ii) + learn_rate * (normalizer * (tft_p_score*(tf-ftf_p)/delta2) - regular * ftf_p);
+                    F_T(fid_n,ii) = F_T(fid_n,ii) + learn_rate * (normalizer * (-tft_n_score*(tf-ftf_n)/delta2) - regular * ftf_n);                    
+                end
+            case 4
+                b = varargin{1};
+                ufu_p_score = GetSigmodPartialLoss(b+MyDot(U(uid,:),F_U(fid_p,:)));
+                ufu_n_score = GetSigmodPartialLoss(b+MyDot(U(uid,:),F_U(fid_n,:)));
+                tft_p_score = GetSigmodPartialLoss(b+MyDot(T(tid,:),F_T(fid_p,:)));
+                tft_n_score = GetSigmodPartialLoss(b+MyDot(T(tid,:),F_T(fid_n,:)));                
+                for ii = 1:num_feature
+                    uf = U(uid,ii);
+                    tf = T(tid,ii);
+                    fuf_p = F_U(fid_p,ii);
+                    fuf_n = F_U(fid_n,ii);
+                    ftf_p = F_T(fid_p,ii);
+                    ftf_n = F_T(fid_n,ii);
+                    U(uid,ii) = U(uid,ii) + learn_rate *(normalizer * (ufu_p_score*fuf_p-ufu_n_score*fuf_n) - regular * uf);
+                    T(tid,ii) = T(tid,ii) + learn_rate *(normalizer * (tft_p_score*ftf_p-tft_n_score*ftf_n) - regular * tf);                    
+                    F_U(fid_p,ii) = F_U(fid_p,ii) + learn_rate * (normalizer * (uf*ufu_p_score) - regular * fuf_p);
+                    F_U(fid_n,ii) = F_U(fid_n,ii) + learn_rate * (normalizer * (-uf*ufu_n_score) - regular * fuf_n);
+                    F_T(fid_p,ii) = F_T(fid_p,ii) + learn_rate * (normalizer * (tf*tft_p_score) - regular * ftf_p);
+                    F_T(fid_n,ii) = F_T(fid_n,ii) + learn_rate * (normalizer * (-tf*tft_n_score) - regular * ftf_n);
+                end
+            otherwise
+                error(['invalid kernel type:' num2str(kernel_type)]);
+        end
+    end
+    function local_ret = MyDot(a,b)
+        local_ret = 0;
+        for ii = 1:num_feature
+            local_ret = local_ret + a(ii) * b(ii);
+        end
+    end
+end
